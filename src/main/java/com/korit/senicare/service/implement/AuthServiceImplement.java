83ff4ebe8202fc6cbd89dcb17175ca5aa7ebb855
nanayeon <tires0301@gmail.com>
2024-09-11 15:57:01 +0900

    3:50

--- src/main/java/com/korit/senicare/service/implement/AuthServiceImplement.java
+++ src/main/java/com/korit/senicare/service/implement/AuthServiceImplement.java
@@ -4,9 +4,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.korit.senicare.common.util.AuthNumberCreator;
 import com.korit.senicare.dto.request.auth.IdCheckRequestDto;
+import com.korit.senicare.dto.request.auth.TelAuthRequestDto;
 import com.korit.senicare.dto.response.ResponseDto;
+import com.korit.senicare.entity.TelAuthNumberEntity;
+import com.korit.senicare.provider.SmsProvider;
 import com.korit.senicare.repository.NurseRepository;
+import com.korit.senicare.repository.TelAuthNumberRepository;
 import com.korit.senicare.service.AuthService;
 
 import lombok.RequiredArgsConstructor;
@@ -15,19 +20,23 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AuthServiceImplement implements AuthService {
 
+    private final SmsProvider smsProvider;
+
     private final NurseRepository nurseRepository;
+    private final TelAuthNumberRepository telAuthNumberRepository;
 
     @Override
     public ResponseEntity<ResponseDto> idCheck(IdCheckRequestDto dto) {
-        
+
         String userId = dto.getUserId();
 
         try {
 
             boolean isExistedId = nurseRepository.existsByUserId(userId);
-            if (isExistedId) return ResponseDto.duplicatedUserId();
+            if (isExistedId)
+                return ResponseDto.duplicatedUserId();
 
-        } catch(Exception exception) {
+        } catch (Exception exception) {
             exception.printStackTrace();
             return ResponseDto.databaseError();
         }
@@ -36,4 +45,37 @@ public class AuthServiceImplement implements AuthService {
 
     }
 
+    @Override
+    public ResponseEntity<ResponseDto> telAuth(TelAuthRequestDto dto) {
+
+        String telNumber = dto.getTelNumber();
+
+        try {
+
+            boolean isExistedTelNumber = nurseRepository.existsByTelNumber(telNumber);
+            if (isExistedTelNumber)
+                return ResponseDto.duplicatedTelNumber();
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return ResponseDto.databaseError();
+        }
+
+        String authNumber = AuthNumberCreator.number4();
+
+        smsProvider.sendMessage(telNumber, authNumber);
+
+        try {
+
+            TelAuthNumberEntity telAuthNumberEntity = new TelAuthNumberEntity(telNumber, authNumber);
+            telAuthNumberRepository.save(telAuthNumberEntity);
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return ResponseDto.databaseError();
+        }
+
+        return ResponseDto.success();
+
+    }
 }
